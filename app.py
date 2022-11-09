76528d155a5cb96adb02a550c09f4e9be3b50c51
AobaIwaki-py <aobaiwaki@gmail.com>
2022-11-09 22:09:41 +0900

    initialization

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import qrcode as qr
+from PIL import Image
+import numpy as np
+
+st.title('QRコード自動生成アプリ')
+
+url = st.text_input('QRコードを生成したいURL:')
+if st.button('QRコード生成'):
+    _img = qr.make(url)
+    _img.save('qrcode.png')
+    img = Image.open('qrcode.png')
+    st.image(img)
